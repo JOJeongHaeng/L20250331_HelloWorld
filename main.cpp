90f1a1a0cd4ec24012b6717ce027cc07bfab1595
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-03-31 16:39:09 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 1;
+    int num2 = 1;
+    float A = 5.5f;
+    int B = (int)A;
+
+    cout << num1 + num2 << endl;
+
+    return 0;
+}
