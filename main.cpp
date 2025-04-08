bb971e9f9f58a83ef06e2a85b4025abc51371891
JeongHaeng Cho <118785061+JOJeongHaeng@users.noreply.github.com>
2025-04-08 17:12:39 +0900

    콘솔에 맵과 플레이어 이동 구현하기

--- main.cpp
+++ main.cpp
@@ -1,22 +1,87 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
-// 이건 오류
+struct PlayerInfo
+{
+    int X;
+    int Y;
+    string Shape;
+};
+
+void GotoXY(int x, int y)
+{
+    COORD Cur;
+    Cur.X = x;
+    Cur.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Cur);
+}
+
 
 int main()
 {
-    enum EColor
-    {
-        red,
-        orange = 4,
-    };
-    int num1 = 1;
-    int num2 = 1;
-    float A = 5.5f;
-    int B = (int)A;
-    EColor Ered(orange);
-    cout << Ered << endl;
+    PlayerInfo* PlayerData;
+    PlayerData = new PlayerInfo;
 
+    PlayerData->X = 5;
+    PlayerData->Y = 5;
+    PlayerData->Shape = "P";
+
+    bool IsRunning = true;
+
+    int input;
+    while (IsRunning)
+    {
+        system("cls");
+        int MapSize = 20;
+        for (int i = 0; i <= MapSize; i++)
+        {
+            for (int j = 0; j <= MapSize; j++)
+            {
+                if (i == 0 || j == 0 || i == MapSize || j == MapSize)
+                {
+                    GotoXY(i, j);
+                    cout << "*";
+                }
+            }
+        }
+        GotoXY(PlayerData->X, PlayerData->Y);
+        cout << PlayerData->Shape;
+        input = _getch();
+        if (input == 77 || toupper(input) == 'D') {  //오른쪽
+            PlayerData->X += 1;
+            if (PlayerData->X == MapSize)
+            {
+                PlayerData->X -= 1;
+            }
+        }
+        else if (input == 75 || toupper(input) == 'A') { //왼쪽
+            PlayerData->X -= 1;
+            if (PlayerData->X == 0)
+            {
+                PlayerData->X += 1;
+            }
+        }
+        else if (input == 72 || toupper(input) == 'W') { //위
+            PlayerData->Y -= 1;
+            if (PlayerData->Y == 0)
+            {
+                PlayerData->Y += 1;
+            }
+        }
+        else if (input == 80 || toupper(input) == 'S') { //아래
+            PlayerData->Y += 1;
+            if (PlayerData->Y == MapSize)
+            {
+                PlayerData->Y -= 1;
+            }
+        }
+        else if (input == 27)
+        {
+            IsRunning = false;
+        }
+    }   
     return 0;
 }
